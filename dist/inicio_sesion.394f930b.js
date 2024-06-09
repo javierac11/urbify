524bc77015ee072ddf6a357ba052d928ca5d0378
javierac11 <javieracedocastrillon@gmail.com>
2024-06-09 22:29:29 +0200

    Terminado

---
+++ dist/inicio_sesion.394f930b.js
@@ -0,0 +1,45 @@
+// script.js
+document.addEventListener("DOMContentLoaded", ()=>{
+    const toggleButton = document.getElementById("toggle-dark-mode");
+    // Restaurar el estado del modo oscuro al cargar la página
+    if (sessionStorage.getItem("dark-mode") === "true") enableDarkMode();
+    else disableDarkMode();
+    toggleButton.addEventListener("click", ()=>{
+        if (document.body.classList.contains("dark-mode")) disableDarkMode();
+        else enableDarkMode();
+    });
+    function enableDarkMode() {
+        document.body.classList.add("dark-mode");
+        document.querySelector("html").classList.add("dark-mode");
+        document.querySelectorAll("button").forEach((button)=>{
+            button.classList.add("dark-mode");
+        });
+        document.querySelector("button.modo").classList.add("dark-mode");
+        document.querySelector("div.iniciar_sesion").classList.add("dark-mode");
+        document.querySelector("footer").classList.add("dark-mode");
+        document.querySelector("header").classList.add("dark-mode");
+        document.querySelectorAll("a").forEach((a)=>{
+            a.classList.add("dark-mode");
+        });
+        toggleButton.textContent = "Modo Claro";
+        sessionStorage.setItem("dark-mode", "true");
+    }
+    function disableDarkMode() {
+        document.body.classList.remove("dark-mode");
+        document.querySelector("html").classList.remove("dark-mode");
+        document.querySelectorAll("button").forEach((button)=>{
+            button.classList.remove("dark-mode");
+        });
+        document.querySelector("button.modo").classList.remove("dark-mode");
+        document.querySelector("div.iniciar_sesion").classList.remove("dark-mode");
+        document.querySelector("footer").classList.remove("dark-mode");
+        document.querySelector("header").classList.remove("dark-mode");
+        document.querySelectorAll("a").forEach((a)=>{
+            a.classList.remove("dark-mode");
+        });
+        toggleButton.textContent = "Modo Oscuro";
+        sessionStorage.setItem("dark-mode", "false");
+    }
+});
+
+//# sourceMappingURL=inicio_sesion.394f930b.js.map
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,23 +1,53 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('toggle-dark-mode');
 
+    if (sessionStorage.getItem('dark-mode') === 'true') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+
     toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        document.querySelector("html").classList.toggle('dark-mode');
-        document.querySelectorAll('button').forEach(button => {
-            button.classList.toggle('dark-mode');
-        });
-        document.querySelector('button.modo').classList.toggle('dark-mode');
-        document.querySelector('.inicio_sesion').classList.toggle('dark-mode');
-        document.querySelector('footer').classList.toggle('dark-mode');
-        document.querySelector('header').classList.toggle('dark-mode');
-        document.querySelectorAll('a').forEach(a => {
-            a.classList.toggle('dark-mode')
-        });
         if (document.body.classList.contains('dark-mode')) {
-            toggleButton.textContent = 'Modo Claro';
+            disableDarkMode();
         } else {
-            toggleButton.textContent = 'Modo Oscuro';
+            enableDarkMode();
         }
     });
+
+    function enableDarkMode() {
+        document.body.classList.add('dark-mode');
+        document.querySelector("html").classList.add('dark-mode');
+        document.querySelectorAll('button').forEach(button => {
+            button.classList.add('dark-mode');
+        });
+        document.querySelector('button.modo').classList.add('dark-mode');
+        document.querySelector('.inicio_sesion').classList.add('dark-mode');
+        document.querySelector('footer').classList.add('dark-mode');
+        document.querySelector('header').classList.add('dark-mode');
+        document.querySelectorAll('a').forEach(a => {
+            a.classList.add('dark-mode');
+        });
+        toggleButton.textContent = 'Modo Claro';
+        sessionStorage.setItem('dark-mode', 'true');
+    }
+
+    function disableDarkMode() {
+        document.body.classList.remove('dark-mode');
+        document.querySelector("html").classList.remove('dark-mode');
+        document.querySelectorAll('button').forEach(button => {
+            button.classList.remove('dark-mode');
+        });
+        document.querySelector('button.modo').classList.remove('dark-mode');
+        document.querySelector('.inicio_sesion').classList.remove('dark-mode');
+        document.querySelector('footer').classList.remove('dark-mode');
+        document.querySelector('header').classList.remove('dark-mode');
+        document.querySelectorAll('a').forEach(a => {
+            a.classList.remove('dark-mode');
+        });
+        toggleButton.textContent = 'Modo Oscuro';
+        sessionStorage.setItem('dark-mode', 'false');
+    }
 });
+
