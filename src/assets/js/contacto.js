0ce28fcacb3414747277981cf21549d4e121919c
javierac11 <javieracedocastrillon@gmail.com>
2024-06-09 18:11:10 +0200

    modo oscuro falta posicionar boton

---
+++ src/assets/js/contacto.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.querySelector("html").classList.toggle('dark-mode');
+        document.querySelectorAll('button').forEach(button => {
+            button.classList.toggle('dark-mode');
+        });
+        document.querySelector('div.contacto').classList.toggle('dark-mode');
+        document.querySelector('footer').classList.toggle('dark-mode');
+        document.querySelector('header').classList.toggle('dark-mode');
+        document.querySelectorAll('a').forEach(a => {
+            a.classList.toggle('dark-mode')
+        });
+        if (document.body.classList.contains('dark-mode')) {
+            toggleButton.textContent = 'Modo Claro';
+        } else {
+            toggleButton.textContent = 'Modo Oscuro';
+        }
+    });
+});
---
+++ src/assets/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        document.querySelector("html").classList.toggle('dark-mode');
+        document.querySelectorAll('button').forEach(button => {
+            button.classList.toggle('dark-mode');
+        });
+        document.querySelector('button.modo').classList.toggle('dark-mode');
+        document.querySelector('.inicio_sesion').classList.toggle('dark-mode');
+        document.querySelector('footer').classList.toggle('dark-mode');
+        document.querySelector('header').classList.toggle('dark-mode');
+        document.querySelectorAll('a').forEach(a => {
+            a.classList.toggle('dark-mode')
+        });
+        if (document.body.classList.contains('dark-mode')) {
+            toggleButton.textContent = 'Modo Claro';
+        } else {
+            toggleButton.textContent = 'Modo Oscuro';
+        }
+    });
+});
